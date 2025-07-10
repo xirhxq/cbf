eeec668d94d93dc4b2039d24b06aa311e4b852be
xirhxq <373836275@qq.com>
2025-07-10 20:45:22 +0800

    feat(cbc): add marker and text annotation for CBC values
    - Add marker and text annotation to CBC plot
    - Update plot initialization and animation to include new elements
    - Improve legend display by removing unnecessary entries

--- scripts/draw/components/cbc.py
+++ scripts/draw/components/cbc.py
@@ -1,5 +1,4 @@
 from utils import *
-
 from .base import BaseComponent
 
 
@@ -49,6 +48,20 @@ class CBCComponent(BaseComponent):
             self.values[f"{cbf_name}"] = {"time": times, "dh": np.array(dh_list), "alphah": np.array(alphah_list)}
 
         self.lines = {}
+        self.markers = {}
+        self.value_texts = {}
+
+        marker_style = dict(marker='*', color='red', alpha=0.7, markersize=10)
+        text_style = dict(color='red', alpha=0.8, fontsize=9, bbox=dict(facecolor='white', alpha=0.3, edgecolor='none'))
+
+        for label in self.values:
+            marker_line, = self.ax.plot([np.nan], [np.nan], **marker_style)
+            self.markers[label] = marker_line
+
+            text = self.ax.text(np.nan, np.nan, "", **text_style,
+                                verticalalignment='center',
+                                horizontalalignment='left')
+            self.value_texts[label] = text
         self._initialize_plot()
 
     def get_abbv(self, cbf_name):
@@ -68,12 +81,6 @@ class CBCComponent(BaseComponent):
             time_data = self.values[label]["time"]
             line, = self.ax.plot(time_data, self.values[label]["dh"] - self.values[label]["alphah"], label=label)
             self.lines[label] = line
-            # for key in self.values[label].keys():
-            #     if key == 'time':
-            #         continue
-            #     label_str = self.get_abbv(label) + '-' + key
-            #     line, = self.ax.plot(time_data, self.values[label][key], label=label_str)
-            #     self.lines[label_str] = line
 
         self.ax.legend(loc='best')
 
@@ -90,3 +97,28 @@ class CBCComponent(BaseComponent):
 
     def update(self, num, dataNow=None):
         self.vline.set_data([self.runtime[num], self.runtime[num]], self.y_limits)
+
+        time_span = self.runtime[self.index_range[1]] - self.runtime[self.index_range[0]]
+        time_offset = time_span * 0.015
+
+        x_limits = self.ax.get_xlim()
+
+        for label, line in self.lines.items():
+            idx = num - self.index_range[0]
+            cbc = self.values[label]['dh'][idx] - self.values[label]['alphah'][idx]
+
+            current_time = self.runtime[num]
+
+            self.markers[label].set_data(current_time, cbc)
+
+            if current_time < (x_limits[0] + x_limits[1]) / 2:
+                self.value_texts[label].set_horizontalalignment('left')
+                self.value_texts[label].set_position((current_time + time_offset, cbc))
+            else:
+                self.value_texts[label].set_horizontalalignment('right')
+                self.value_texts[label].set_position((current_time - time_offset, cbc))
+
+            # 设置文本内容
+            self.value_texts[label].set_text(f"{self.get_abbv(label)}: {cbc:.4f}")
+
+        self.ax.legend(loc='best')
